ed3b8222476d7ab972e538d38795727bb5a0cdcc
knblomquist00 <blo18004@byui.edu>
2024-04-09 01:25:55 -0600

    fixes

--- scripts/rentals.js
+++ scripts/rentals.js
@@ -1,32 +1,32 @@
 const baseURL = "https://kblomquist01.github.io/final/";
-const linksURL = "https://kblomquist01.github.io/final/data/members.json";
+const linksURL = "https://kblomquist01.github.io/final/data/rentals.json";
 
-const cards = document.querySelector('#cards');
+const cards = document.querySelector('#rentals');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
-    displayLinks(data.members);
+    displayLinks(data.Rentals);
   }
 
   const displayLinks = (members) => {
     members.forEach((member) => {
       
 
-        let description = document.createElement('tr')
+        let description = document.createElement('tr');
     
         
-        fullName.textContent = `${member.name}`;
   
-       description.innerHTML = 
-       `<td>${member.name}</td>
-       <td">${member.maxPersons}</td>
-       <td">${member.Reservation.halfDay}</td>
-       <td">${member.Reservation.fullDay}</td>
-       <td">${member.WalkIn.halfDay}</td>
-       <td">${member.WalkIn.halfDay}</td>`;
-    
+       description.innerHTML = `
+       <td>${member.name}</td>
+       <td>${member.maxPersons}</td>
+       <td>${member.Reservation.halfDay}</td>
+       <td>${member.Reservation.fullDay}</td>
+       <td>${member.WalkIn.halfDay}</td>
+       <td>${member.WalkIn.fullDay}</td>`;
+       console.log(description);
+
       
         cards.appendChild(description);
       }); 
